78dd6555b93801242e05b7b42ebb9e3c51f43281
mur7ay <mur7ay@me.com>
2016-04-30 22:29:28 -0400

    Added user settings.

--- Movie_Informer/app/src/main/java/com/example/android/movie_informer/MainActivity.java
+++ Movie_Informer/app/src/main/java/com/example/android/movie_informer/MainActivity.java
@@ -1,20 +1,59 @@
 package com.example.android.movie_informer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static boolean TABLET = false;
+    public boolean isTablet(Context context)
+    {
+        boolean xlarge = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK)==4);
+        boolean large = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK)==Configuration.SCREENLAYOUT_SIZE_LARGE);
+        return(xlarge||large);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Replacing current fragment with new fragment not continually adding new fragments.
+        TABLET=isTablet(this);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new MoviesFragment()).commit();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, new MoviesFragment())
+                    .commit();
+        }
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+
+            return true;
         }
 
+        return super.onOptionsItemSelected(item);
     }
 
 
--- Movie_Informer/app/src/main/java/com/example/android/movie_informer/MoviesFragment.java
+++ Movie_Informer/app/src/main/java/com/example/android/movie_informer/MoviesFragment.java
@@ -1,18 +1,20 @@
 package com.example.android.movie_informer;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,11 +24,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by shawnmurray on 4/27/16.
@@ -37,37 +38,47 @@ public class MoviesFragment extends Fragment {
     static GridView gridview;
     static int width;
     static ArrayList<String> posters;
-    static boolean sortByPopularity;
-    static final String API_KEY = "292045ea04fcdc083fb7908532fbd563";
+    static boolean sortByPop;
+    static String API_KEY = "292045ea04fcdc083fb7908532fbd563";
+
+    static PreferenceChangeListener listener;
+    static SharedPreferences sharedPreferences;
+    static boolean sortByFavorites;
+    static ArrayList<String> postersFav = new ArrayList<>();
 
     public MoviesFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        // Inflate the layout for this fragment
 
-        if (getActivity() != null) {
+//        WindowManager wm =(WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+//        Display display = wm.getDefaultDisplay();
+//        Point size = new Point();
+//        display.getSize(size);
+//        if(MainActivity.TABLET)
+//        {
+//            width = size.x/6;
+//        }
+//        else width=size.x/3;
+        if(getActivity()!=null)
+        {
             ArrayList<String> array = new ArrayList<>();
-            ImageAdapter adapter = new ImageAdapter(getActivity(), array, width);
-            gridview = (GridView) gridview.findViewById(R.id.gridview);
+            ImageAdapter adapter = new ImageAdapter(getActivity(),array,width);
+            gridview = (GridView)rootView.findViewById(R.id.gridview);
 
-            gridview.setColumnWidth(2);
+            gridview.setColumnWidth(width);
             gridview.setAdapter(adapter);
-
         }
-
+        //listen for presses on gridview items
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("Position", "" + position);
-                //Intent intent = new Intent(MoviesFragment.this, MovieDetails.class);
-                //intent.putExtra("Position", mThumbIds[position]);
-                //startActivity(intent);
+                System.out.println(position);
             }
         });
 
@@ -75,93 +86,150 @@ public class MoviesFragment extends Fragment {
         return rootView;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        getActivity().setTitle("Most Popular Movies");
+    private class PreferenceChangeListener implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-        if (isNetworkAvailable()) {
-            gridview.setVisibility(gridview.VISIBLE);
-            new ImageLoadTask().execute();
-        } else {
-            Toast.makeText(getContext(), "Sorry, network isn't available", Toast.LENGTH_SHORT).show();
-            gridview.setVisibility(GridView.GONE);
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            gridview.setAdapter(null);
+            onStart();
+        }
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        listener = new PreferenceChangeListener();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(listener);
+
+        if (sharedPreferences.getString("sortby", "popularity").equals("popylarity")) {
+            getActivity().setTitle("Most Popular Movies");
+
+            sortByPop = true;
+            sortByFavorites = false;
+
+        } else if (sharedPreferences.getString("sortby", "popularity").equals("rating")) {
+            getActivity().setTitle("Highest Rated");
+
+            sortByPop = false;
+            sortByFavorites = false;
+        } else if (sharedPreferences.getString("sortby", "popularity").equals("favorites")) {
+            getActivity().setTitle("Favorite Movies");
+
+            sortByPop = false;
+            sortByFavorites = true;
         }
 
+        TextView textView = new TextView(getActivity());
+        RelativeLayout layout = (RelativeLayout)getActivity().findViewById(R.id.relativelayout);
+
+        if (sortByFavorites) {
+            if (postersFav.size() == 0) {
+                textView.setText("You have no favorited movies.");
+
+                if (layout.getChildCount() == 1)
+                    layout.addView(textView);
+                gridview.setVisibility(GridView.GONE);
+
+            } else {
+                gridview.setVisibility(GridView.VISIBLE);
+                layout.removeView(textView);
+            }
+
+            if (postersFav != null && getActivity() !=  null) {
+                ImageAdapter adapter = new ImageAdapter(getActivity(), postersFav, width);
+                gridview.setAdapter(adapter);
+            }
+        }
+
+        else {
+            gridview.setVisibility(GridView.VISIBLE);
+            layout.removeView(textView);
+
+
+            if (isNetworkAvailable()) {
+                //gridview.setVisibility(GridView.VISIBLE);
+                new ImageLoadTask().execute();
+            } else {
+                TextView textview1 = new TextView(getActivity());
+                RelativeLayout layout1 = (RelativeLayout) getActivity().findViewById(R.id.relativelayout);
+                textview1.setText("You are not connected to the Internet");
+                if (layout1.getChildCount() == 1) {
+                    layout1.addView(textview1);
+                }
+                gridview.setVisibility(GridView.GONE);
+            }
+        }
     }
 
-    public boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+    public boolean isNetworkAvailable()
+    {
+        ConnectivityManager connectivityManager = (ConnectivityManager)getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        return activeNetworkInfo !=null &&activeNetworkInfo.isConnected();
     }
-
-    // The last parameter is what is returned so an arraylist of strings is return.
     public class ImageLoadTask extends AsyncTask<Void, Void, ArrayList<String>> {
 
-        // String of poster paths
         @Override
         protected ArrayList<String> doInBackground(Void... params) {
-            while (true) {
-                try {
-                    posters = new ArrayList(Arrays.asList(getPathsFromAPI(sortByPopularity)));
+            while(true){
+                try{
+                    posters = new ArrayList(Arrays.asList(getPathsFromAPI(sortByPop)));
                     return posters;
-                } catch (Exception e) {
+                }
+                catch(Exception e)
+                {
                     continue;
                 }
-
             }
-        }
 
+        }
         @Override
-        protected void onPostExecute(ArrayList<String> result) {
+        protected void onPostExecute(ArrayList<String>result)
+        {
+            if(result!=null && getActivity()!=null)
+            {
+                ImageAdapter adapter = new ImageAdapter(getActivity(),result, width);
+                gridview.setAdapter(adapter);
 
-            if (result != null && getActivity() != null) {
-                ImageAdapter imageAdapter = new ImageAdapter(getActivity(), result, width);
-                gridview.setAdapter(imageAdapter);
             }
-            super.onPostExecute(result);
         }
-
-        // returning a string array.
-        public String[] getPathsFromAPI(boolean sortByPopularity) {
-
-            while(true) {
-                HttpsURLConnection urlConnection = null;
+        public String[] getPathsFromAPI(boolean sortbypop)
+        {
+            while(true)
+            {
+                HttpURLConnection urlConnection = null;
                 BufferedReader reader = null;
                 String JSONResult;
 
                 try {
                     String urlString = null;
-                    if (sortByPopularity) {
-                        urlString = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + API_KEY;
+                    if (sortbypop) {
+                        urlString = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + API_KEY;
                     } else {
-                        urlString = "https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count." +
-                                "gte=500&api_key=" + API_KEY;
+                        urlString = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&vote_count.gte=500&api_key=" + API_KEY;
                     }
                     URL url = new URL(urlString);
-                    urlConnection = (HttpsURLConnection)url.openConnection();
+                    urlConnection = (HttpURLConnection) url.openConnection();
                     urlConnection.setRequestMethod("GET");
                     urlConnection.connect();
 
-                    // Reading input stream into a String
+                    //Read the input stream into a String
                     InputStream inputStream = urlConnection.getInputStream();
                     StringBuffer buffer = new StringBuffer();
-
                     if (inputStream == null) {
                         return null;
                     }
-
                     reader = new BufferedReader(new InputStreamReader(inputStream));
                     String line;
-
                     while ((line = reader.readLine()) != null) {
                         buffer.append(line + "\n");
                     }
                     if (buffer.length() == 0) {
                         return null;
                     }
-                    // All of the results will be in this result.
                     JSONResult = buffer.toString();
 
                     try {
@@ -169,38 +237,41 @@ public class MoviesFragment extends Fragment {
                     } catch (JSONException e) {
                         return null;
                     }
-                } catch (Exception e) {
+                }catch(Exception e)
+                {
                     continue;
-                } finally {
-                    if (urlConnection != null) {
+                }finally {
+                    if(urlConnection!=null)
+                    {
                         urlConnection.disconnect();
                     }
-                    if (reader != null) {
-                        try {
+                    if(reader!=null)
+                    {
+                        try{
                             reader.close();
-                        } catch (final IOException e) {
-
+                        }catch(final IOException e)
+                        {
                         }
                     }
                 }
+
+
             }
         }
         public String[] getPathsFromJSON(String JSONStringParam) throws JSONException {
 
             JSONObject JSONString = new JSONObject(JSONStringParam);
 
-            JSONArray moviesArray = JSONString.getJSONArray("result");
+            JSONArray moviesArray = JSONString.getJSONArray("results");
             String[] result = new String[moviesArray.length()];
 
-            for (int i = 0; i < moviesArray.length(); i++) {
+            for(int i = 0; i<moviesArray.length();i++)
+            {
                 JSONObject movie = moviesArray.getJSONObject(i);
                 String moviePath = movie.getString("poster_path");
                 result[i] = moviePath;
-
             }
             return result;
         }
     }
-
-
 }
